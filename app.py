1d08bae263a1c06a6c03d86a0f1f2ce0f188f835
Doha Guettioui <D.guettioui@aui.ma>
2025-02-12 01:08:17 +0100

    my app

--- app.py
+++ app.py
@@ -6,10 +6,26 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import plot_confusion_matrix, plot_roc_curve, plot_precision_recall_curve
-from sklearn.metrics import precision_score, recall_score 
+from sklearn.metrics import ConfusionMatrixDisplay, RocCurveDisplay, PrecisionRecallDisplay
+
 
 def main():
+    #documentation
+    #st.markdown
+    st.title("Binary Classification Web App")
+    st.sidebar.title("Binary Classification Web App")
+    st.markdown("Are your mushrooms edible or pisonous? 🍄")
+    st.sidebar.markdown("Are your mushrooms edible or pisonous? 🍄")
+    
+    def load_data():
+        data = pd.read_csv('mushrooms.csv')
+        label = LabelEncoder()
+        for col in data.columns:
+            data[col] = label.fit_transform(data[col])
+        return data
+
+    df = load_data()
+            
     
 
 if __name__ == '__main__':
